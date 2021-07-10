8348808626769bcf588f8eb8f0d1862ba2e1f18b
Yuri Kitchin <ykitchin@gmail.com>
2021-07-10 19:15:37 +0800

    fix path names, to display index.html

--- Develop/server.js
+++ Develop/server.js
@@ -5,8 +5,11 @@ const path = require('path');
 const app = express()
 const PORT = 3001;
 
+//add static files. this allows express to serve your static files such as CSS and javascript
+app.use(express.static("./Develop/public"))
+
 //Routes
-app.get('/notes', (req, res) => res.sendFile('./Develop/public/index.html', { root: __dirname } ));
+app.get('/', (req, res) => res.sendFile('./public/index.html', { root: __dirname } ));
 
 //begins server listening on designated port
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
