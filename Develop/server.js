5296a3eb6ec16a3bd6d140003ebb7e25bb9ce0fa
Yuri Kitchin <ykitchin@gmail.com>
2021-07-10 19:09:36 +0800

    add nodemon, and first route

--- Develop/server.js
+++ Develop/server.js
@@ -1,2 +1,12 @@
-var express = require('express')
-var app = express()
+//Declare dependancies
+const express = require('express')
+const fs = require("fs");
+const path = require('path');
+const app = express()
+const PORT = 3001;
+
+//Routes
+app.get('/notes', (req, res) => res.sendFile('./Develop/public/index.html', { root: __dirname } ));
+
+//begins server listening on designated port
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
