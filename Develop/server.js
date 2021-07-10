e16bdd5ad2f6fb68a81fdcff89a91f9f924e5b9b
Yuri Kitchin <ykitchin@gmail.com>
2021-07-10 20:00:03 +0800

    rename file paths to get CSS and JS to work

--- Develop/server.js
+++ Develop/server.js
@@ -6,10 +6,12 @@ const app = express()
 const PORT = 3001;
 
 //add static files. this allows express to serve your static files such as CSS and javascript
-app.use(express.static("./Develop/public"))
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.get('/', (req, res) => res.sendFile('./public/index.html', { root: __dirname } ));
+
+app.get('/notes', (req, res) => res.sendFile('./public/notes.html', { root: __dirname } ));
 
 //begins server listening on designated port
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
