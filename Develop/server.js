9c9f54646d7d3777974fd20ca074f5867e908b77
Yuri Kitchin <ykitchin@gmail.com>
2021-07-13 09:16:23 +0800

    add route to handle save route

--- Develop/server.js
+++ Develop/server.js
@@ -1,43 +1,59 @@
 //Declare dependancies
-const { json } = require('express');
-const express = require('express')
+const { json } = require("express");
+const express = require("express");
 const fs = require("fs");
-const util = require('util')
-const readFile = util.promisify(fs.readFile)
-const bodyParser = require('body-parser')
-const path = require('path');
-const app = express()
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const path = require("path");
+const app = express();
 const PORT = 3001;
 
 //add static files. this allows express to serve your static files such as CSS and javascript
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 //add body parser - parse url encoded information into a ne wbody object and converts it to JSON
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
-app.get('/', (req, res) => res.sendFile('./public/index.html', { root: __dirname } ));
+app.get("/", (req, res) =>
+  res.sendFile("./public/index.html", { root: __dirname })
+);
 
-app.get('/notes', (req, res) => res.sendFile('./public/notes.html', { root: __dirname } ));
+app.get("/notes", (req, res) =>
+  res.sendFile("./public/notes.html", { root: __dirname })
+);
 
 //get api router response from client side
-app.get('/api/notes', async (req, res) => {
-    let notes = await readFile('./db/db.json');
-    notes = JSON.parse(notes)
-    return res.json(notes)
-})
+app.get("/api/notes", async (req, res) => {
+  let notes = await readFile("./db/db.json");
+  notes = JSON.parse(notes);
+  return res.json(notes);
+});
 
-app.post('/api/notes', async (req, res) => {
-    const newNote = req.body
-    let notes = await readFile('./db/db.json')
-    notes = JSON.parse(notes)
-    notes.push(newNote)
-    console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", notes)
-    
-   
+//saves user input and returns new db.json
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
 
-})
+  fs.readFile("./db/db.json", (error, data) => {
+    if (error) throw error;
+    let notesArray = JSON.parse(data);
+    notesArray.push(newNote);
+    console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", notesArray);
+
+    fs.writeFile("./db/db.json", JSON.stringify(notesArray), function (err) {
+      console.log("bbbbbbbbbbbbbbbbbbbbbbbbbb", JSON.stringify(notesArray));
+      if (err) throw err;
+      else {
+        console.log("thew new note has been added");
+        res.sendStatus(200);
+      }
+    });
+  });
+});
+
+//delete route
 
 //begins server listening on designated port
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
